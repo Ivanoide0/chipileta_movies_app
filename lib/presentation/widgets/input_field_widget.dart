5b0fcd6329107c315d9f22760dd15724305a19b8
Ivanoide0 <202300126@upqroo.edu.mx>
2026-07-03 17:40:42 -0500

    CAMBIO DE MENSAJE DE ERRORES EN REGISTER

--- lib/presentation/widgets/input_field_widget.dart
+++ lib/presentation/widgets/input_field_widget.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:chipileta_movies_app/resources/colors/colors.dart';
-import 'package:chipileta_movies_app/resources/styles/styles.dart';
 
 class AppInputField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  final String? Function(String?) validator;
   final bool isPassword;
   final bool obscureText;
   final VoidCallback? onToggleObscure;
-  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
   final bool enabled;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextCapitalization textCapitalization;
 
   const AppInputField({
     super.key,
     required this.controller,
     required this.hintText,
-    required this.validator,
     this.isPassword = false,
     this.obscureText = false,
     this.onToggleObscure,
-    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.keyboardType,
     this.enabled = true,
+    this.inputFormatters,
+    this.textCapitalization = TextCapitalization.none,
   });
 
   @override
@@ -29,37 +34,90 @@ class AppInputField extends StatelessWidget {
     return TextFormField(
       controller: controller,
       enabled: enabled,
+      validator: validator,
       keyboardType: keyboardType,
       obscureText: isPassword ? obscureText : false,
-      style: AppStyles.inputText,
-      validator: validator,
+      inputFormatters: inputFormatters,
+      textCapitalization: textCapitalization,
+      style: const TextStyle(
+        color: AppColors.white,
+        fontSize: 15,
+        fontWeight: FontWeight.w600,
+      ),
+      cursorColor: AppColors.yellow,
       decoration: InputDecoration(
         hintText: hintText,
-        hintStyle: AppStyles.inputHint,
+        hintStyle: TextStyle(
+          color: AppColors.white.withValues(alpha: 0.58),
+          fontSize: 15,
+          fontWeight: FontWeight.w500,
+        ),
+
+        filled: true,
+        fillColor: AppColors.backgroundDark.withValues(alpha: 0.35),
+
         contentPadding: const EdgeInsets.symmetric(
           horizontal: 18,
-          vertical: 14,
+          vertical: 17,
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-          borderSide: const BorderSide(
-            color: AppColors.white,
-            width: 2,
+
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18),
+          borderSide: BorderSide(
+            color: AppColors.white.withValues(alpha: 0.20),
+            width: 1.2,
           ),
         ),
+
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18),
+          borderSide: BorderSide(
+            color: AppColors.white.withValues(alpha: 0.20),
+            width: 1.2,
+          ),
+        ),
+
         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(18),
+          borderSide: const BorderSide(
+            color: AppColors.turquoise,
+            width: 1.8,
+          ),
+        ),
+
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18),
           borderSide: const BorderSide(
             color: AppColors.yellow,
-            width: 2,
+            width: 1.8,
           ),
         ),
+
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18),
+          borderSide: const BorderSide(
+            color: AppColors.yellow,
+            width: 2.1,
+          ),
+        ),
+
+        errorMaxLines: 3,
+
+        errorStyle: const TextStyle(
+          color: AppColors.yellow,
+          fontSize: 12.5,
+          fontWeight: FontWeight.w700,
+          height: 1.25,
+        ),
+
         suffixIcon: isPassword
             ? IconButton(
-                onPressed: enabled ? onToggleObscure : null,
+                onPressed: onToggleObscure,
                 icon: Icon(
-                  obscureText ? Icons.visibility_off : Icons.visibility,
-                  color: AppColors.yellow,
+                  obscureText
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                  color: AppColors.white70,
                 ),
               )
             : null,
